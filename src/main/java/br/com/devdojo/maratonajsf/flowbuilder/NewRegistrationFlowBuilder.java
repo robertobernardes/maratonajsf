758e12cadc0bf59c28fbdea31766ccfbb64f5662
Roberto Bernardes <roberto.bernardes@inema.ba.gov.br>
2018-01-25 09:16:54 -0300

    Aula 20 - Escopos pt 09, FlowScoped pt 04, FlowBuilder, Flows com anotações

---
+++ src/main/java/br/com/devdojo/maratonajsf/flowbuilder/NewRegistrationFlowBuilder.java
@@ -0,0 +1,32 @@
+package br.com.devdojo.maratonajsf.flowbuilder;
+
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.builder.FlowDefinition;
+import java.io.Serializable;
+
+
+public class NewRegistrationFlowBuilder implements Serializable{
+	
+	@Produces
+	@FlowDefinition
+	public Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder) {
+		String flowId = "newregistration";
+		flowBuilder.id("", flowId);
+		flowBuilder.viewNode(flowId, "/newregistration/newregistration.xhtml").markAsStartNode();
+		flowBuilder.viewNode(flowId, "/newregistration/newregistration2.xhtml");
+		flowBuilder.viewNode(flowId, "/newregistration/newregistration3.xhtml");
+		
+		flowBuilder.switchNode("newRegistrationPage2")
+			.defaultOutcome(flowId)
+			.switchCase()
+			.condition("#{not empty testeFlowBuilderBean.nome and not empty testeFlowBuilderBean.sobrenome}")
+			.fromOutcome("newregistration2");
+		
+		flowBuilder.returnNode("exitToInicio").fromOutcome("/inicioflow.xhtml");
+		flowBuilder.returnNode("exitToIndex").fromOutcome("/index.xhtml");
+		return flowBuilder.getFlow();
+	}
+}
